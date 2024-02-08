76e8731d63846b8bb5a31b5ade94e95954d3b030
Htoo Aung Lin <59634121+Htoo23@users.noreply.github.com>
2024-02-08 10:37:05 +0630

    finished updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyB9A1aREy4fGZxFqHWe1u2rpGTMK7SJpUQ",
   authDomain: "react-auth-tutorial-12a44.firebaseapp.com",
@@ -14,8 +12,8 @@ const firebaseConfig = {
   appId: "1:982000206619:web:7b20e85b68a551fec049be"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Initalize Firebase Authentication and get a reference to the service
-const auth=getAuth(app);
+
+export const auth = getAuth(app);
